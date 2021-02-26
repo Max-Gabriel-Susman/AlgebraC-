a326f8570758fd2678a01000404ebcc49b42f06b
Max-Gabriel-Susman <gabe.susman@gmail.com>
2021-02-25 19:19:44 -0700

    added logic for distance between two points formula

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AlgebraC__
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Math time baby!");
+
+            double DistanceBetweenTwoPoints(double x1, double y1, double x2, double y2)
+            {
+                double distance = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+
+                return distance;
+            }
+
+            Console.WriteLine(DistanceBetweenTwoPoints(2,3,4,5));
+        }
+    }
+}
